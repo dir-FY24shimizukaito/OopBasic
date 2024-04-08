169ad4c98c2f4885d57d77b5ae30ef984004f31f
dir-FY24shimizukaito <kaito.shimizu.u257@dirbato.co.jp>
2024-04-08 15:54:39 +0900

    Lesson3 Chapter5まで

--- OopBasic/src/company/Employee.java
+++ OopBasic/src/company/Employee.java
@@ -2,14 +2,14 @@ package company;
 
 public class Employee {
     // フィールド
-    String name;
-    String department;
-    String position;
-    int employeeId;
+    protected final String name;
+    private final Department department;
+    private final String position;
+    private final int employeeId;
     
     
     // コンストラクター
-    public Employee(String name, String department, String position, int employeeId) {
+    public Employee(String name, Department department, String position, int employeeId) {
         this.name = name;
         this.department = department;
         this.position = position;
@@ -26,5 +26,14 @@ public class Employee {
     public void report() {
         report(1);
     }
+    
+    
+    // 会議に参加するメソッド
+    public void joinMeeting() {
+        department.meeting();
+        System.out.println("→上記の会議に参加します。部署 ： " + department.getName() + "名前 ： " + name);
+        
+    }
+    
 
 }
