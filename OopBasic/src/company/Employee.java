263ebde1fb7ad4cd50c7cef269e36ac8b84e6c6e
dir-FY24shimizukaito <kaito.shimizu.u257@dirbato.co.jp>
2024-04-09 11:14:07 +0900

    Lesson4 Chapter3 インターフェイス

--- OopBasic/src/company/Employee.java
+++ OopBasic/src/company/Employee.java
@@ -1,14 +1,16 @@
 package company;
 
-public class Employee {
-    // フィールド
+
+public class Employee implements Workable {
+
+    // フィールド(属性)
     protected final String name;
     private final Department department;
     private final String position;
     private final int employeeId;
     
     
-    // コンストラクター
+    // コンストラクター(初期化)
     public Employee(String name, Department department, String position, int employeeId) {
         this.name = name;
         this.department = department;
@@ -33,6 +35,13 @@ public class Employee {
         department.meeting();
         System.out.println("→上記の会議に参加します。部署 ： " + department.getName() + "名前 ： " + name);
         
+    }
+    
+    
+    // 働くメソッドを実装
+    @Override
+    public void work() {
+        System.out.println("正社員として働きます。 名前 ： " + name + slogan);
     }
     
 
